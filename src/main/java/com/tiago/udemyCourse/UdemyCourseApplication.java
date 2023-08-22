e31b3805e83a3a17cb47fac615f7553ee3ca1d0a
tiagovst <tiago.vassan@gmail.com>
2023-08-22 12:58:48 -0300

    Project Created

---
+++ src/main/java/com/tiago/udemyCourse/UdemyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.udemyCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCourseApplication.class, args);
+	}
+
+}
